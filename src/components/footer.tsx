97da0ed9bea2a118666cecc02fb5c154c97a233b
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-09-19 21:32:28 -0300

    fix: 🐛 change transitionY value

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -29,41 +29,41 @@ export function Footer() {
                                 />
                             </div>
                             <div className="flex flex-col">
-                                <span className="text-3xl font-semibold text-neutral-100">Gabriel S. Cordeiro</span>
+                                <span className="text-3xl font-semibold text-neutral-50">Gabriel S. Cordeiro</span>
                                 <span className="text-xl text-neutral-500">Senior front-end developer</span>
                             </div>
                         </div>
 
-                        <div className="mt-10 flex items-center gap-6">
-                            <Link href="https://www.linkedin.com/in/gabriel-schmidt-cordeiro-199262103/" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-10px]">
+                        <div className="mt-5 flex items-center gap-6">
+                            <Link href="https://www.linkedin.com/in/gabriel-schmidt-cordeiro-199262103/" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-5px]">
                                 <FaLinkedin size={26} className="text-white"/>
                             </Link>
 
-                            <Link href="https://github.com/gabrielscordeiro" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-10px]">
+                            <Link href="https://github.com/gabrielscordeiro" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-5px]">
                                 <FaGithub size={26}  className="text-white"/>
                             </Link>
 
-                            <Link href="https://stackoverflow.com/users/4798156/gabriel-schmidt-cordeiro" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-10px]">
+                            <Link href="https://stackoverflow.com/users/4798156/gabriel-schmidt-cordeiro" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-5px]">
                                 <FaStackOverflow size={26}  className="text-white"/>
                             </Link>
 
-                            <Link href="https://stackoverflow.com/users/4798156/gabriel-schmidt-cordeiro" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-10px]">
+                            <Link href="https://stackoverflow.com/users/4798156/gabriel-schmidt-cordeiro" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-5px]">
                                 <FaInstagram  size={26}  className="text-white"/>
                             </Link>
 
-                            <Link href="mailto:gabrielcordeiro.dev@gmail.com" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-10px]">
+                            <Link href="mailto:gabrielcordeiro.dev@gmail.com" rel="nofollow" target="_blank" className="transition-transform delay-75 duration-150 hover:translate-y-[-5px]">
                                 <BiLogoGmail size={26}  className="text-white"/>
                             </Link>
                         </div>
                     </div>
 
                     <div>
-                        <h4 className="flex items-center gap-4 text-5xl font-semibold text-neutral-100">
+                        <h4 className="flex items-center gap-4 text-5xl font-semibold text-neutral-50">
                             Get in touch
                             <FaArrowRight className="text-blue-700"/>
                         </h4>
 
-                        <div className="mt-10 flex flex-col">
+                        <div className="mt-5 flex flex-col">
                             <h5 className="font-semibold uppercase text-neutral-400">
                                 Email me:
                             </h5>
@@ -72,7 +72,7 @@ export function Footer() {
                                 <Link
                                     href="mailto:gabrielcordeiro.dev@gmail.com"
                                     target="_blank"
-                                    className="mt-2 inline-flex items-center gap-2 border-b-2 border-neutral-500 text-xl font-semibold leading-10 transition-colors hover:border-neutral-300"
+                                    className="mt-2 inline-flex items-center gap-2 border-b-2 border-neutral-500 text-xl font-semibold leading-5 transition-colors hover:border-neutral-300"
                                 >
                                     gabrielcordeiro.dev@gmail.com <FaArrowRight />
                                 </Link>
@@ -85,7 +85,7 @@ export function Footer() {
                     <ul className="group flex gap-8 text-xl font-semibold text-neutral-200">
                         {menu.map((item) => (
                             <li key={item.link}>
-                                <Link href={item.link} className="text-md text-neutral-300 transition-colors hover:text-neutral-100">
+                                <Link href={item.link} className="text-md text-neutral-300 transition-colors hover:text-neutral-50">
                                     {item.name}
                                 </Link>
                             </li>
